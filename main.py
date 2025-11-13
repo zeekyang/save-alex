bc164ee7d71e3ab3fc718d6489184469c88b5f63
Zek Yang <zekai_yang@berkeley.edu>
2025-11-13 00:11:32 -0800

    first steps

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Save Alex
+# hello
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Save Alex")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+pygame.quit()
